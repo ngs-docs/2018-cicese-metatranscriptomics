5d8b958dbefa06fbc8eb2288a8699049919202fe
C. Titus Brown <titus@idyll.org>
2018-11-08 09:23:29 -0800

    update plot-gather

--- scripts/plot-gather.py
+++ scripts/plot-gather.py
@@ -1,7 +1,10 @@
 #! /usr/bin/env python
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
 import pyupset as pyu
 import matplotlib as mpl
-import matplotlib.pyplot as plt
 from pickle import load
 import pandas as pd
 import glob 
@@ -11,9 +14,10 @@ for file in glob.glob('*csv'):
     df=pd.read_csv(file, delimiter = ",")
     x=file.split('.')[0]
     genus_dict[x]=df
+    print(x)
 
 genus_dict['reads'] = genus_dict.pop('ERR1719497_paired_gather_all')
-genus_dict['assembly'] = genus_dict.pop('tara_f135_megahit_all_gather-scaled10k-k31')
+genus_dict['assembly'] = genus_dict.pop('tara_f135_full_megahit')
 
 pplot=pyu.plot(genus_dict, unique_keys = ['name'])
 pplot['figure'].savefig('plot-gather.png')
